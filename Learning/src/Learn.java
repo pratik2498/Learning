3095b58da1399ab4ed1ffc0d68bbe56977e0f8a8
Pratik <pratik2498@gmail.com>
2023-02-23 03:02:41 +0530

    Second Commit

--- Learning/src/Learn.java
+++ Learning/src/Learn.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
 
 public class Learn {
 	public static void main(String args[])
 	{
 		System.out.println("Print java");
+		String s1="java"; String s2 ="";
+		char [] ch = s1.toCharArray();
+		Arrays.sort(ch);
+		for(char ele : ch)
+		{
+			s2=s2+ele;
+		}
+		System.out.println(s2);
 	}
 
 }
